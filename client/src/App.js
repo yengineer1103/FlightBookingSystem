b0fd67233c91e1363d842cc8e8515679aae8acea
yengineer1103 <yashviengineerr11703@gmail.com>
2024-03-04 22:27:55 +0530

    Last commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import TopNav from './components/TopNav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <div style={{minHeight:'10vh', width:'100%',background:'rgb(25 118 210/55%)'}}></div>
+      <TopNav/>
       </header>
+      <div className='main-body'>
+        <h1>Welcome to my booking website</h1>
+      </div>
+      <footer>
+        <div style={{minHeight:'10vh', width:'100%',background:'rgb(25 118 210/55%)'}}></div>
+      </footer>
     </div>
   );
 }
